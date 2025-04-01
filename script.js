7f76a6f0439c044f42e44fb790eac54ad095a574
maria elis <147657602+MariaAlberton@users.noreply.github.com>
2025-04-01 13:30:45 -0300

    .

--- script.js
+++ script.js
@@ -12,6 +12,8 @@ botaoDeAcessibilidade.addEventListener('click', function (){
     const aumentaFonteBotao = document.getElementById('aumentar-fonte');
     const diminuiFonteBotao = document.getElementById('diminuir-fonte');
 
+    const alternaContraste = document.getElementById('alterna-contraste')
+
     let tamanhoAtualFonte = 1;
     aumentaFonteBotao.addEventListener('click', function(){
         tamanhoAtualFonte += 0.1;
@@ -24,6 +26,9 @@ botaoDeAcessibilidade.addEventListener('click', function (){
         document.body.style.fontSize = `${tamanhoAtualFonte}rem`
 
     })
+    alternaContraste.addEventListener('click', function(){
+        document.body.classList.toggle('alto-contraste')
+})
 
 })
 
